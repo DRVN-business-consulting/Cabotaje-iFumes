adc27ca2ab8cd7dfdba3fdd3133a81de3a83c7e0
Jay Cabotaje <73106289+fordcabotaje@users.noreply.github.com>
2024-09-09 09:03:11 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import { MusicPlayerContextProvider } from './src/providers/music_provider';
+import { ThemeContextProvider } from './src/providers/theme_provider';
+import MainScreen from './src/screens/main';
+
+export default function App() {
+   return (
+    <ThemeContextProvider>
+      <MusicPlayerContextProvider>
+        <MainScreen/>
+      </MusicPlayerContextProvider>
+    </ThemeContextProvider>
+  );
+}
